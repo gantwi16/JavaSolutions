88d3c290e67a950310fc01c130e4ead59c556f68
gantwi16 <GAntwi@qa.com>
2021-06-25 15:21:22 +0100

    added new files

--- Cmembers.java
+++ Cmembers.java
@@ -4,4 +4,5 @@ public class Cmembers {
 	public static void main(String[] args) {
 		StringEx2.method2("today is Friday");
 	}
+	
 }
--- Enhanced.java
+++ Enhanced.java
@@ -1,5 +1,32 @@
 package com.qa.helloworld;
 
 public class Enhanced {
-	
+	public static void main(String[] args) {
+		String [] strings = {"I", "am", "hungry."};
+		for (String word : strings ) {
+			System.out.println(word);
+		}
+		
+		int [] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+		for (int i : numbers) {
+			System.out.println(i * i);
+		}
+		
+		for (int i : numbers) {
+            if (oddoreven(i)) {
+                System.out.println("Even: " + i * i);
+            } else {
+                System.out.println("Odd: " + i * i * i);
+            }
+        }
+	}
+    public static boolean oddoreven(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
